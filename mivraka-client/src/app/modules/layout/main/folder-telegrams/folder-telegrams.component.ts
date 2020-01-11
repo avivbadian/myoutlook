ee61f0f6f992a3a29c636c039ea0004044c1bdf3
Aviv Badian <avivbadian@gmail.com>
2020-01-11 16:26:20 +0200

    mat-list

--- mivraka-client/src/app/modules/layout/main/folder-telegrams/folder-telegrams.component.ts
+++ mivraka-client/src/app/modules/layout/main/folder-telegrams/folder-telegrams.component.ts
@@ -1,9 +1,14 @@
 import { TelegramsService } from './../../../core/telegrams.service';
-import { Component, OnInit, OnDestroy, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ViewChild, HostListener } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
 import { Telegram } from 'src/app/modules/models/telegram';
-import { jqxGridComponent } from 'jqwidgets-ng/jqxgrid';
 
+export enum KEY_CODE {
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  RIGHT_ARROW = 39,
+  DOWN_ARROW = 40,
+}
 
 @Component({
   selector: 'app-folder-telegrams',
@@ -11,32 +16,47 @@ import { jqxGridComponent } from 'jqwidgets-ng/jqxgrid';
   styleUrls: ['./folder-telegrams.component.scss']
 })
 export class FolderTelegramsComponent implements OnInit, OnDestroy {
-  @ViewChild('grid', {static: false}) myGrid: jqxGridComponent;
   telegrams$: Observable<Telegram[]>;
   telegrams: Telegram[];
+  selectedTelegrams: number[];
+  selectedRowIndex: number;
   currentFolder: number;
   subscription: Subscription;
   dataAdapter: any;
-
-  fruits: string[] = ['Apple', 'Orange', 'Banana'];
 
   constructor(
     private telegramsService: TelegramsService
   ) { }
 
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (event.keyCode === KEY_CODE.DOWN_ARROW) {
+        
+    }
+  }
+
   ngOnInit() {
+    this.selectedTelegrams = [];
     this.subscription = this.telegramsService.currentFolder$.subscribe(
       currentFolder => {
         this.currentFolder = currentFolder;
         this.telegrams$ = this.telegramsService.getTelegrams(currentFolder);
-        this.telegrams$.subscribe(te => {
-        });
       });
-
   }
 
   ngOnDestroy(): void {
     // tslint:disable-next-line: no-unused-expression
     this.subscription && this.subscription.unsubscribe();
+  }
+
+  telegramSelected(id: number, rowIndex: number, numOfRows: number) {
+    this.selectedTelegrams = [id];
+    if (rowIndex >= 0 && rowIndex <= numOfRows - 1) {
+      this.selectedRowIndex = rowIndex;
+    }
+  }
+
+  checkSelected(id: number) {
+    return this.selectedTelegrams.indexOf(id) > -1;
   }
 }
